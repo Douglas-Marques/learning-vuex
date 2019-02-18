24cee02aeda841a5ce3d8bb4784e63e0e0bf6627
Douglas.Marques <douglas.marques@dbccompany.com.br>
2019-02-18 14:12:34 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <product-list-one></product-list-one>
+    <product-list-two></product-list-two>
+  </div>
+</template>
+
+<script>  
+  import ProductListOne from './components/ProductListOne'
+  import ProductListTwo from './components/ProductListTwo'
+
+  export default {
+    name: 'app',
+    components: {
+      ProductListOne,
+      ProductListTwo
+    }
+  }
+</script>
+
+<style>
+  body {
+    font-family: Ubuntu;
+    color: #555;
+  }
+</style>
